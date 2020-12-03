ad817aa0c1013e9efeed4bdd657108758e6ce895
felix tineo <ftandcompany@gmail.com>
2020-12-03 08:28:40 -0400

    card image fix

--- src/_components/cards/property.js
+++ src/_components/cards/property.js
@@ -28,7 +28,7 @@ const CardCont = styled.div`
   }
 `
 const CardImage = styled.div`
-  background-image: url(${props => props.src});
+  background-image: url("${props => props.src}");
   background-position: center;
   background-size: cover;
   background-repeat: none;
